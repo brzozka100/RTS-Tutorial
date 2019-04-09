bed974b1c9a7ecce6be8e67512538d658dffc50d
brzozka100 <brzozka100@gmail.com>
2019-04-09 21:44:57 +0200

    S3W10: Camera Controll

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraControl : MonoBehaviour
+{
+    public float cameraSpeed, groundHeight, zoomSpeed;
+    public Vector2 cameraHeightMinMax, cameraRotationMinMax;
+    [Range(0, 1)]
+    public float zoomLerp = .1f;
+    [Range(0, 0.2f)]
+    public float cursorTreshold;
+
+    RectTransform selectionBox;
+    new Camera camera;
+    Vector2 mousePos, mousePosScreen, keyboardInput, mouseScroll;
+    bool isCursorInGameScreen;
+
+
+    private void Awake()
+    {
+        selectionBox = GetComponentInChildren<Image>(true).transform as RectTransform;
+        camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+        UpdateZoom();
+        UpdateClicks();
+    }
+
+    
+
+    void UpdateMovement()
+    {
+        keyboardInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        mousePos = Input.mousePosition;
+        mousePosScreen = camera.ScreenToViewportPoint(mousePos);
+        isCursorInGameScreen = mousePosScreen.x >= 0 && mousePosScreen.x <=1 &&
+            mousePosScreen.y >= 0 && mousePosScreen.y <= 1;
+
+        Vector2 movementDirection = keyboardInput;
+
+        if (isCursorInGameScreen)
+        {
+            if (mousePosScreen.x < cursorTreshold) movementDirection.x -= 1 - mousePosScreen.x / cursorTreshold;
+            if (mousePosScreen.x > 1 - cursorTreshold) movementDirection.x += 1 - (1 - mousePosScreen.x) / cursorTreshold;
+            if (mousePosScreen.y < cursorTreshold) movementDirection.y -= 1 - mousePosScreen.y / cursorTreshold;
+            if (mousePosScreen.y > 1 - cursorTreshold) movementDirection.y += 1 - (1 - mousePosScreen.y) / cursorTreshold;
+        }
+
+        var deltaPosition = new Vector3(movementDirection.y, 0, -movementDirection.x);
+        deltaPosition *= cameraSpeed * Time.deltaTime;
+        transform.position += deltaPosition;
+    }
+
+    void UpdateZoom()
+    {
+        var mouseScroll = Input.mouseScrollDelta;
+        float zoomDelta = mouseScroll.y * zoomSpeed * Time.deltaTime;
+        zoomLerp = Mathf.Clamp01(zoomLerp + zoomDelta);
+
+        var position = transform.localPosition;
+        position.y = Mathf.Lerp(cameraHeightMinMax.y, cameraHeightMinMax.x, zoomLerp) + groundHeight;
+        transform.localPosition = position;
+
+        var rotation = transform.localEulerAngles;
+        rotation.x = Mathf.Lerp(cameraRotationMinMax.y, cameraRotationMinMax.x, zoomLerp);
+        transform.localEulerAngles = rotation;
+        
+
+        //todo
+    }
+
+    void UpdateClicks()
+    {
+        //   selectionBox.anchoredPosition = mousePos;
+        //todo
+    }
+}
